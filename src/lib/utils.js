2e25cbf2956e713001c7f9d48b56e30586fff181
IbraAguero <ibrahimaguero@gmail.com>
2024-08-08 21:06:15 -0300

    fix: limit decimal display to two places

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,8 +9,8 @@ const conversions = {
   unidades: 1,
   mililitros: 1,
   gramos: 1,
-  taza: 240, // 1 taza = 240 gramos
-  media_taza: 120, // 1/2 taza = 120 gramos
+  taza: 200, // 1 taza = 240 gramos
+  media_taza: 100, // 1/2 taza = 120 gramos
   cucharada: 15, // 1 cucharada = 15 gramos
   cucharadita: 5, // 1 cucharadita = 5 gramos
 };
@@ -19,5 +19,5 @@ export function getPrice(ingredient) {
   const { measure, quantity, pricePerUnit } = ingredient;
   const quantityInGrams = quantity * conversions[measure];
   const price = (quantityInGrams / conversions["gramos"]) * pricePerUnit;
-  return price;
+  return price.toFixed(2);
 }
